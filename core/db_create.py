527b1abc5c607e10f12ac713f338a0d06c327338
Richard9306 <113300542+Richard9306@users.noreply.github.com>
2023-12-01 23:53:18 +0100

    creating database

---
+++ core/db_create.py
@@ -0,0 +1,25 @@
+import sqlite3
+
+conn = sqlite3.connect('individuals.db')
+cursor = conn.cursor()
+sql_script = """
+CREATE TABLE IF NOT EXISTS individuals (
+    individual_id INTEGER PRIMARY KEY AUTOINCREMENT,
+    firstname TEXT,
+    telephone_number TEXT,
+    email TEXT,
+    password TEXT,
+    role TEXT,
+    created_at DATETIME
+);
+CREATE TABLE IF NOT EXISTS children (
+    child_id INTEGER PRIMARY KEY AUTOINCREMENT,
+    individual_id INTEGER NOT NULL,
+    name TEXT,
+    age INTEGER,
+    FOREIGN KEY (individual_id) REFERENCES individuals(individual_id)
+);
+"""
+cursor.executescript(sql_script)
+conn.commit()
+conn.close()
---
+++ core/script.py
@@ -0,0 +1,32 @@
+import sqlite3
+import json
+from pathlib import Path
+
+conn = sqlite3.connect('individuals.db')
+cursor = conn.cursor()
+with open(Path('data_json.json'), 'r') as json_file:
+    data = json.load(json_file)
+    for individual_data in data['individuals']:
+        cursor.execute("""
+        INSERT INTO individuals (firstname, telephone_number, email, password, role, created_at)
+        VALUES (?, ?, ?, ?, ?, ?)
+        """, (
+            individual_data['firstname'],
+            individual_data['telephone_number'],
+            individual_data['email'],
+            individual_data['password'],
+            individual_data['role'],
+            individual_data['created_at']
+        ))
+        parent_id = cursor.lastrowid
+        for child_data in individual_data['children']:
+            cursor.execute("""
+            INSERT INTO children (parent_id, name, age)
+            VALUES (?, ?, ?)
+            """, (
+                parent_id,
+                child_data['name'],
+                child_data['age']
+            ))
+conn.commit()
+conn.close()
